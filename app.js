ae974cbb1b93ece1c880869067b61c89c6cfa293
Andy <andyxie976@gmail.com>
2021-03-15 09:55:18 +0100

    added timer 4.0 added animation --> transition effect

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ const navSlide=()=>{
 }
 navSlide();
 let cardsList = []; // List of all created Cards
-let lockBoard = false
-let secondClick = false
-let startedGame = false
+let lockBoard = false;
+let secondClick = false;
+let startedGame = false;
 let firstCard;
 let secondCard;
-let time = 30000;
+let time = 30000 * 0.6 // equals 30 seconds 0.6 is just a factor
 let cal_time;
 let numMatching = 0;
 
@@ -29,22 +29,22 @@ $("#reset_btn").prop('disabled', true)
 $("#reset_btn").on('click',function(){
     $(".memory-game").empty();
     $(".memory-game").append("<div class='card1' id='card'><img src='img/agent.png\'><h3>Who is the spy?</h3><p>Text</p></div>");
-    $("#select_btn").prop('disabled',false);
+    $("#start_btn").prop('disabled',false);
     cardsList = [];
     numMatching = 0;
     clearInterval(cal_time);
     time = 30000;
-    document.getElementById("timer").style.color ="#ec5252";
-    document.getElementById("timer").innerHTML ="";
+    document.getElementById("timer").style.color = "#ec5252";
+    document.getElementById("timer").innerHTML = "";
     $("#Num_Card").val("");
     $("#reset_btn").prop('disabled', true)
 })
-$("#select_btn").on('click',function (){
+$("#start_btn").on('click',function (){
    let inputNum = $("#Num_Card").val();
-    if((inputNum!="")&&(inputNum%2===0)){
+    if((inputNum!="")&&(inputNum%2 === 0)){
         $(".card1").remove();
         inputNum = inputNum/2
-       for(let i=0;i<inputNum;i++){
+       for(let i=0; i < inputNum; i++){
            let mathProblem = generateRandomMathProblem()
            createCard(i,mathProblem+" = ");
            createCard(i, Math.round(eval(mathProblem) * 100) / 100);
@@ -52,13 +52,14 @@ $("#select_btn").on('click',function (){
         $("#Num_Card").val("");
         let shuffled = shuffle(cardsList)
         $(".memory-game").append(shuffled);
-        $("#select_btn").prop('disabled',true);
+        $("#start_btn").prop('disabled',true);
         $("#reset_btn").prop('disabled', false);
         cal_time = setInterval(function() {
-            time = time - 1000;
-            let seconds = Math.floor((time % (1000 * 60)) / 1000);
-            let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-            document.getElementById("timer").innerHTML ="Timer: "+minutes+"m "+seconds+ "s "
+            time = time - 10;
+            let milSec = time - Math.floor(time / 100) * 100;
+            let seconds = Math.floor((time % (1000 * 60 * 60)) / (10 * 60));
+            document.getElementById("timer").innerHTML ="Timer: "+seconds+"s "+milSec+ "ms "
+            console.log(time);
             if (time == 0) {
                 clearInterval(cal_time);
                 lockBoard = true;
@@ -66,10 +67,10 @@ $("#select_btn").on('click',function (){
             }
             if(numMatching === cardsList.length/2){
                 clearInterval(cal_time);
-                document.getElementById("timer").innerHTML ="Timer: "+minutes+"m "+seconds+ "s => Success";
+                document.getElementById("timer").innerHTML ="Timer: "+seconds+"s "+milSec+ "ms => Success";
                 document.getElementById("timer").style.color ="darkseagreen";
             }
-        }, 1000);
+        }, 10);
         console.log(shuffled)
         console.log($(".memory-card").get())
     }
@@ -102,7 +103,7 @@ function flipCard(elem){
         }
     }
 }
-function match_checking(f_card,s_card){
+function match_checking(f_card, s_card){
     if(f_card.id === s_card.id){
         console.log("Match gefunden");
         f_card.setAttribute("match","true")
@@ -111,6 +112,8 @@ function match_checking(f_card,s_card){
         console.log(s_card.getAttribute("match"))
         f_card.style.color = "#2eff00";
         s_card.style.color = "#2eff00";
+        f_card.style.cursor= "default";
+        s_card.style.cursor= "default";
         numMatching = numMatching + 1;
     }
     else{
@@ -126,24 +129,24 @@ function match_checking(f_card,s_card){
         },1000)
     }
 }
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(inputArray) {
+    let currentIndex = inputArray.length, temporaryValue, randomIndex;
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = inputArray[currentIndex];
+        inputArray[currentIndex] = inputArray[randomIndex];
+        inputArray[randomIndex] = temporaryValue;
     }
-    return array;
+    return inputArray;
 }
 
 function getRandomInt(min,max){
     let result = (Math.round(Math.random()*max)+min)
-    if(result<0){result= "("+result+")"}
+    if(result < 0){result = "("+result+")"} //brackets for negative numbers
     return (result)
 }
 function generateRandomMathProblem(){
@@ -154,7 +157,7 @@ function generateRandomMathProblem(){
     for(let i = 0; i < num; i++){
         equation = equation + getRandomInt(minValue,maxValue) + randomSign()
     }
-    equation = equation+getRandomInt(minValue,maxValue)
+    equation = equation + getRandomInt(minValue,maxValue)
     return equation;
 }
 
